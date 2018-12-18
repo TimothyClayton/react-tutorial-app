3ffb22599f0cc9198ad185aa36e75e4f2c1f47bd
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2018-12-18 16:04:52 -0700

    Adds test for square's post-click text

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -6,6 +6,48 @@ it('renders without crashing', () => {
   shallow(<Game />)
 })
 
+it('sets the appropriate text of a clicked square', () => {
+  let squares = Array(9).fill(null)
+  const onClick = jest.fn()
+  let wrapper = mount(<Game />)
+
+  const square1 = wrapper.find('button.square').at(0)
+  square1.simulate('click')
+  expect(square1.text()).toEqual('X')
+
+  const square2 = wrapper.find('button.square').at(3)
+  square2.simulate('click')
+  expect(square2.text()).toEqual('O')
+
+  const square3 = wrapper.find('button.square').at(6)
+  square3.simulate('click')
+  expect(square3.text()).toEqual('X')
+
+  const square4 = wrapper.find('button.square').at(8)
+  square4.simulate('click')
+  expect(square4.text()).toEqual('O')
+
+  const square5 = wrapper.find('button.square').at(5)
+  square5.simulate('click')
+  expect(square5.text()).toEqual('X')
+
+  const square6 = wrapper.find('button.square').at(2)
+  square6.simulate('click')
+  expect(square6.text()).toEqual('O')
+
+  const square7 = wrapper.find('button.square').at(1)
+  square7.simulate('click')
+  expect(square7.text()).toEqual('X')
+
+  const square8 = wrapper.find('button.square').at(4)
+  square8.simulate('click')
+  expect(square8.text()).toEqual('O')
+
+  const square9 = wrapper.find('button.square').at(7)
+  square9.simulate('click')
+  expect(square9.text()).toEqual('X')
+})
+
 it('renders game status correctly', () => {
   const wrapper = mount(<Game />)
   const gameInfoTurn1 = wrapper.find('div.game-info').children().first().text()
