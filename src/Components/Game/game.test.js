ff65c4861316fd4fdd5750b246335795f350e87e
Timothy Clayton <timothy.daniel.clayton@gmail.com>
2018-12-18 15:30:28 -0700

    Reorders directories and finishes specs

---
+++ src/Components/Game/game.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Game from './game'
+import {shallow, mount} from 'enzyme'
+
+it('renders without crashing', () => {
+  shallow(<Game />)
+})
+
+it('renders game status correctly', () => {
+  const wrapper = mount(<Game />)
+  const gameInfoTurn1 = wrapper.find('div.game-info').children().first().text()
+  expect(gameInfoTurn1).toEqual('Next player: X')
+
+  const button = wrapper.find('button.square').first()
+  button.simulate('click')
+  const gameInfoTurn2 = wrapper.find('div.game-info').children().first().text()
+  expect(gameInfoTurn2).toEqual('Next player: O')
+
+  const turn2 = wrapper.find('button.square').at(1)
+  turn2.simulate('click')
+
+  const turn3 = wrapper.find('button.square').at(4)
+  turn3.simulate('click')
+
+  const turn4 = wrapper.find('button.square').at(5)
+  turn4.simulate('click')
+
+  const turn5 = wrapper.find('button.square').at(8)
+  turn5.simulate('click')
+
+  const winner = wrapper.find('div.game-info').children().first().text()
+  expect(winner).toEqual('Winner: X')
+})
